2e5fffd7b2bf97d5bce08cc8790e9550b36abb26
SamukaPinho <samuelpinho105@gmail.com>
2022-08-02 12:44:59 -0300

    primeiro commit

---
+++ js/js.js
@@ -0,0 +1,54 @@
+function scrn1(){
+	document.getElementById('heroclass').className = "vis";
+	document.getElementById('mainmenu').className = "inv";
+}
+function backToMenu(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "vis";
+}
+function backToClass(){
+	document.getElementById('heroclass').className = "vis";
+	document.getElementById('fighterid').className = "inv";
+	document.getElementById('mageid').className = "inv";
+	document.getElementById('marksmanid').className = "inv";
+	document.getElementById('assassinid').className = "inv";
+	document.getElementById('tankid').className = "inv";
+	document.getElementById('supportid').className = "inv";
+	document.getElementById('btnBackClass').className = "inv";
+}
+function fighter(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('fighterid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
+function mage(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('mageid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
+function marksman(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('marksmanid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
+function assassin(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('assassinid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
+function tank(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('tankid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
+function support(){
+	document.getElementById('heroclass').className = "inv";
+	document.getElementById('mainmenu').className = "inv";
+	document.getElementById('supportid').className = "vis";
+	document.getElementById('btnBackClass').className = "vis";
+}
